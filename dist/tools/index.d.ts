885f6c7dcf70da6e7eb58be935824b50e05271c1
Phuong To <phuongwd@gmail.com>
2026-01-22 18:42:35 +0700

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/tools/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { createWorktreeTool, createWorktreeSchema, type CreateWorktreeInput } from './create-worktree.js';
-export { listWorktreesTool, listWorktreesSchema, type ListWorktreesInput } from './list-worktrees.js';
-export { cleanupWorktreeTool, cleanupWorktreeSchema, type CleanupWorktreeInput } from './cleanup-worktree.js';
-export { switchWorktreeTool, switchWorktreeSchema, type SwitchWorktreeInput } from './switch-worktree.js';
-export { createPrTool, createPrSchema, type CreatePrInput } from './create-pr.js';
-//# sourceMappingURL=index.d.ts.map
